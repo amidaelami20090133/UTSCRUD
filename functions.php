569546bf10b1e546878f45966ed8e648cc4cee1c
Ami Daelami Ichsana <72344619+amidaelami20090133@users.noreply.github.com>
2022-06-09 13:56:28 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function pdo_connect_mysql() {
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'root';
     $DATABASE_PASS = '';
-    $DATABASE_NAME = 'UTS';
+    $DATABASE_NAME = 'uts';
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
